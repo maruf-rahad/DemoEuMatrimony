c4fabe82b5de65c69db855f5048081df379c2295
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2024-12-24 17:14:40 +0100

    Initial commit with the packages

---
+++ src/test/java/com/eu/demomatrimony/DemomatrimonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eu.demomatrimony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomatrimonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
